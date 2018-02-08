ed6eced36acf3cf8e71defdfa85895dba5ec8099
Johnny Carlson <johncarlson@protonmail.com>
2018-02-08 12:35:57 -0500

    Implement basic Gdax market purchase

--- handler.js
+++ handler.js
@@ -1,16 +1,34 @@
 'use strict';
 
+const Gdax = require('gdax');
+const sandboxApiUrl = 'https://api-public.sandbox.gdax.com';
+const purchasePrice = process.env.INVESTMENT_AMOUNT;
+const productId = process.env.PRODUCT_ID || 'BTC-USD';
+
+var authorizedClient;
+if (process.env.TEST) {
+    console.log('Using sandbox url')
+	authorizedClient = new Gdax.AuthenticatedClient(process.env.GDAX_KEY,
+		                                            process.env.GDAX_SECRET,
+		                                            process.env.GDAX_PASSPHRASE,
+		                                            sandboxApiUrl);
+} else {
+	authorizedClient = new Gdax.AuthenticatedClient(process.env.GDAX_KEY,
+		                                            process.env.GDAX_SECRET,
+		                                            process.env.GDAX_PASSPHRASE);
+}
+
+
+
 module.exports.start = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  authorizedClient.buy({
+    "funds": purchasePrice,
+    "type": "market",
+    "side": "buy",
+    "product_id": productId
+    }).then(data => {
+    	console.log('Purchased ' + productId + ' for ' + purchasePrice);
+    }).catch(error => {
+    	console.log(error);
+    })
 };
